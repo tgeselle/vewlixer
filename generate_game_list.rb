4d62dd3919f3078cb8d1e8ebafbce72a26f3fde8
Thomas Geselle <thomas.geselle@tinuiti.com>
2021-09-19 18:26:03 -0700

    Game list generator working

--- generate_game_list.rb
+++ generate_game_list.rb
@@ -5,19 +5,35 @@ require 'sqlite3'
 class GenerateGameList
   def initialize
     @db = Sequel.connect('sqlite://GameDB.db')[:games]
-    @gamefile = File.open('AttractMode/romlists/GameList.txt', 'w+')
+    @gamefile = File.open('AM/romlists/GameList.txt', 'w+')
+    @rocketlauncher = File.open('Rocketlauncher/Modules/PCLauncher/PCLauncher.ini', 'w+')
   end
 
-  def add_game(game)
+  def process_game(game)
+    add_game_to_am_list game
+    create_teknoparrot_bash_script(game) unless game.teknoparrot_script.nil?
+    add_game_to_rocketlauncher game
+  end
+
+  def add_game_to_am_list(game)
     entry = "#{game.name};#{game.name};#{game.platform};;#{game.year};#{game.studio};#{game.genre};#{game.players};#{game.version}"
     entry = "#{entry};#{game.buttons};;;;#{game.platform};#{game.alt_name};"
     @gamefile.puts entry
   end
 
+  def add_game_to_rocketlauncher(game)
+    @rocketlauncher.puts "[#{game.name}]"
+    @rocketlauncher.puts "Application=..\\Games\\TeknoParrot\\#{game.name}.bat"
+    @rocketlauncher.puts "ahk_class #{game.rocketlauncher_class}"
+    @rocketlauncher.puts ""
+  end
+
+  def create_teknoparrot_bash_script(game)
+    bash = "..\\..\\TeknoParrot\\TeknoParrotUi.exe --profile=#{game.teknoparrot_script}.xml"
+    File.write("Games/TeknoParrot/#{game.name}.bat", bash, mode: 'w+')
+  end
+
   def run
-    @db.order(:name).all.each{ add_game OpenStruct.new(_1) }
+    @db.where(active: 1).order(:name).all.each{ process_game OpenStruct.new(_1) }
   end
 end
-
-
-GenerateGameList.new.run
---
+++ main.rb
@@ -0,0 +1,3 @@
+require_relative 'generate_game_list'
+
+GenerateGameList.new.run
